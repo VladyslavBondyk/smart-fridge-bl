7f3f87e1583184823593dd499143c117eb58010c
BondykV <vladyslav.bondyk@gmail.com>
2022-08-14 19:23:51 +0200

    init db

---
+++ src/main/java/com/spdu/SmartFridgeApplication.java
@@ -0,0 +1,13 @@
+package com.spdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFridgeApplication.class, args);
+	}
+
+}
